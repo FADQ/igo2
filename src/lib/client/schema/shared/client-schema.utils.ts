87a7b06b6dfa045a995404bfca1266f34d9c41b5
cbourget <charlesericbourget@gmail.com>
2019-05-07 12:44:48 -0400

    feat(schema): display modification date in the schema drop list

--- src/lib/client/schema/shared/client-schema.utils.ts
+++ src/lib/client/schema/shared/client-schema.utils.ts
@@ -1,8 +1,18 @@
+import { formatDate } from 'src/lib/utils/date';
+
 import { ClientSchema } from './client-schema.interfaces';
 
 export function getClientSchemaTitle(schema: ClientSchema): string {
+  const parts = [schema.id, schema.type];
+
   if (schema.type === 'PLP') {
-    return `${schema.id} - ${schema.type} - ${schema.annee}`;
+    parts.push(schema.annee);
   }
-  return `${schema.id} - ${schema.type}`;
+
+  const date = (schema.timbreMaj || {date: undefined}).date;
+  if (date !== undefined) {
+    parts.push(formatDate(date));
+  }
+
+  return parts.join(' - ');
 }
