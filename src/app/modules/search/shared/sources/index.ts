ed6fc42b80aa60e10927941cf60b5a4349db7255
cbourget <charlesericbourget@gmail.com>
2019-06-07 14:07:45 -0400

    feat(search): search by coordinates and add locations to icherche

--- src/app/modules/search/shared/sources/index.ts
+++ src/app/modules/search/shared/sources/index.ts
@@ -1,5 +1,6 @@
 export * from './client';
 export * from './client.interfaces';
+export * from './coordinates.providers';
 export * from './icherche';
 export * from './icherche.providers';
 export * from './query.providers';
