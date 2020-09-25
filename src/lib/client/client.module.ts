7e11205ac7d4ecd4fff201a9b20a41bd3b496a4c
cbourget <charlesericbourget@gmail.com>
2020-09-25 15:19:25 -0400

    wip: fix conflicts

--- src/lib/client/client.module.ts
+++ src/lib/client/client.module.ts
@@ -8,15 +8,9 @@ import { provideClientService } from './shared/client.providers';
 import { FadqLibClientInfoModule } from './info/client-info.module';
 import { FadqLibClientParcelModule } from './parcel/client-parcel.module';
 import { FadqLibClientParcelElementModule } from './parcel-element/client-parcel-element.module';
-<<<<<<< HEAD
 import { FadqLibClientParcelTxModule } from './parcel-tx/client-parcel-tx.module';
-<<<<<<< HEAD
-=======
 import { FadqLibClientParcelProModule } from './parcel-pro/client-parcel-pro.module';
->>>>>>> e6d63713... wip: proto scaffolding and update tool
-=======
-import { FadqLibClientParcelProModule } from './parcel-pro/client-parcel-pro.module';
->>>>>>> 7bddd8f0... wip: proto scaffolding and update tool
+
 import { FadqLibClientSchemaModule } from './schema/client-schema.module';
 import { FadqLibClientSchemaFileModule } from './schema-file/client-schema-file.module';
 import { FadqLibClientSchemaElementModule } from './schema-element/client-schema-element.module';
@@ -29,24 +23,10 @@ import { FadqLibClientSchemaElementModule } from './schema-element/client-schema
     FadqLibClientParcelModule.forRoot(),
     FadqLibClientParcelElementModule.forRoot(),
     FadqLibClientParcelTxModule.forRoot(),
+    FadqLibClientParcelProModule.forRoot(),
     FadqLibClientSchemaModule.forRoot(),
     FadqLibClientSchemaFileModule.forRoot(),
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 38d6f19c... wip: proto scaffolding and update tool
     FadqLibClientSchemaElementModule.forRoot()
-=======
-    FadqLibClientSchemaElementModule.forRoot(),
-    FadqLibClientParcelElementModule.forRoot(),
-    FadqLibClientParcelProModule.forRoot()
->>>>>>> e6d63713... wip: proto scaffolding and update tool
-=======
-    FadqLibClientSchemaElementModule.forRoot(),
-    FadqLibClientParcelElementModule.forRoot(),
-    FadqLibClientParcelProModule.forRoot()
->>>>>>> 7bddd8f0... wip: proto scaffolding and update tool
   ],
   exports: [
     FadqLibClientInfoModule,
--- src/lib/client/index.ts
+++ src/lib/client/index.ts
@@ -4,13 +4,6 @@ export * from './schema';
 export * from './schema-file';
 export * from './schema-element';
 export * from './parcel-element';
-<<<<<<< HEAD
 export * from './parcel-tx';
-<<<<<<< HEAD
-=======
 export * from './parcel-pro';
->>>>>>> e6d63713... wip: proto scaffolding and update tool
-=======
-export * from './parcel-pro';
->>>>>>> 7bddd8f0... wip: proto scaffolding and update tool
 export * from './shared';
