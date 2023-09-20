f32330227803969a2837513c1e52521de9beb1a0
danlav01 <daniel.lavallee@fadq.qc.ca>
2023-09-20 10:09:17 -0400

    Few changes to cadastre search (#27029)

--- src/lib/cadastre-reno/shared/cadastre-reno.ts
+++ src/lib/cadastre-reno/shared/cadastre-reno.ts
@@ -44,6 +44,11 @@ export class CadastreRenoSearchSource extends SearchSource implements TextSearch
     };
   }
 
+  /**
+   * Computes request params
+   * @param term A cadastre number to search
+   * @returns Parameters for http structure
+   */
   private computeRequestParams(term: string): HttpParams {
     return new HttpParams({
       fromObject: Object.assign(
@@ -58,22 +63,58 @@ export class CadastreRenoSearchSource extends SearchSource implements TextSearch
 
   /**
    * Search a cadastre by num
-   * @param term Cadastre num
+   * @param term Cadastre number. Could be a list of cadastre numbers separated by «;»
    * @returns Observable of <SearchResult<Cadastre>[]
    */
   search(term?: string): Observable<SearchResult<CadastreRenoFeature>[]> {
-    const params = this.computeRequestParams(term);
+    let blnSearch: boolean = false;
+    let termRevisedText: string;
+    // if there's more than one cadastre number to search
+    if (term.search(';')) {
+      const terms = term.split(';');
+      let tabTermsRevised:string[] = [];
+      // Keep just numbers from each term
+      terms.forEach((noCadastre: string) => {
+        const noCadastreNumber = noCadastre.replace(/[^0-9]/g, '');
+        // if cadastre number is greater or equal to 5, keep it
+        if (noCadastreNumber.length >= 5) {
+          tabTermsRevised.push(noCadastreNumber);
+        }
+      });
+      // if there's at least one cadastre number
+      if (tabTermsRevised.length >= 1) {
+        // Transform to text
+        termRevisedText = tabTermsRevised.join(';');
+        // There's a search to do
+        blnSearch = true;
+      }
+    } 
+    // There's only one cadastre number
+    else {
+      // Keep just numbers
+      termRevisedText = term.replace(/[^0-9]/g, '');
+      // There's a search to do if there's at least 5 digit to cadastre number
+      if (termRevisedText.length >= 5) { blnSearch = true; }
+    }
 
-    if (term.length < 7) { return of([]); }
+    // If there's a search to do
+    if (blnSearch) {
+      const params = this.computeRequestParams(termRevisedText);
 
-    return this.http
-    .get(this.searchUrl, {
-      params,
-      responseType: 'text'
-    })
-    .pipe(map((response: string) => this.extractResults(response)));
+      return this.http
+      .get(this.searchUrl, {
+        params,
+        responseType: 'text'
+      })
+      .pipe(map((response: string) => this.extractResults(response)));
+    } else { return of([]); }
   }
 
+  /**
+   * Extracts results from the response of service
+   * @param response 
+   * @returns results 
+   */
   private extractResults(response: string): SearchResult<CadastreRenoFeature>[] {
     const textResults = response.split('<br />');
     return textResults
@@ -81,6 +122,11 @@ export class CadastreRenoSearchSource extends SearchSource implements TextSearch
       .filter((result: SearchResult<CadastreRenoFeature>) => result !== undefined);
   }
 
+  /**
+   * Convert text result of response to data structure SearchResult
+   * @param cadastre 
+   * @returns SearchResult of CadastreRenoFeature
+   */
   private dataToResult(cadastre: string): SearchResult<CadastreRenoFeature> {
     const propertiesCadastre = cadastre.split(';');
     if (propertiesCadastre.length < 7) {
@@ -95,11 +141,11 @@ export class CadastreRenoSearchSource extends SearchSource implements TextSearch
         geometry: geometry as FeatureGeometry,
         extent: undefined,
         properties: {
-          noCadastre: propertiesCadastre[0]
+          "Numéro de Cadastre": propertiesCadastre[0]
         },
         meta: {
           id: this.getId(),
-          title: 'data.properties.recherche'
+          title: 'Cadastre rénové'
         }
       },
       meta: {
@@ -111,6 +157,11 @@ export class CadastreRenoSearchSource extends SearchSource implements TextSearch
     };
   }
 
+  /**
+   * Converts a WKT format to Geojson format
+   * @param wkt Data to WKT format
+   * @returns Data to Geojson format
+   */
   private convertWKTtoGeojson(wkt: string): GeoJSONGeometry {
     const olFormatWKT = new olFormat.WKT();
     const olFormatGeoJson = new olFormat.GeoJSON();
