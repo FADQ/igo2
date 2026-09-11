3f4ce273bc61beb7f1dfc66cd897334a6ea6c2b4
danlav01 <daniel.lavallee@fadq.qc.ca>
2026-09-11 12:01:44 -0400

    Problème affichage des contrôles du formulaire
    Les contrôles du formulaire ne s'affiche pas et le mode de saisie de géométrie n'est pas activé.

--- src/lib/edition/edition-redraw/edition-redraw.module.ts
+++ src/lib/edition/edition-redraw/edition-redraw.module.ts
@@ -5,6 +5,11 @@ import { MatTabsModule } from '@angular/material/tabs';
 
 import { IgoLanguageModule } from '@igo2/core/language';
 import { IgoFeatureFormModule } from '@igo2/geo';
+import {
+  IgoFormFieldModule,
+  IgoFormGroupModule
+} from '@igo2/common/form';
+
 
 import { FadqLibMessageInlineModule } from 'src/lib/message/message-inline/message-inline.module';
 import { FadqLibCardPanelModule } from 'src/lib/misc/card-panel/card-panel.module';
@@ -21,6 +26,8 @@ import { EditionRedrawComponent } from './edition-redraw.component';
     MatTabsModule,
     IgoLanguageModule,
     IgoFeatureFormModule,
+    IgoFormFieldModule,
+    IgoFormGroupModule,
     FadqLibMessageInlineModule,
     FadqLibCardPanelModule
   ],
